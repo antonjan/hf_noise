34d0389e90da37098b4ac4c2d8a7738869af5198
Anton <antonjan@users.noreply.github.com>
2017-07-14 19:42:34 +0200

    The shel script that gets called from crontab

--- create_heatmap.sh
+++ create_heatmap.sh
@@ -1,4 +1,3 @@
-#This script will create an image of the 1 to 30Mhz band from a CSV "/home/anton/hf_noise/hf_monitoring_`date '+%d-%m-%Y'`.cvs" witch will create a png image "hf_noise_`date '+%d-%m-%Y'`.png"
 sudo /home/anton/Downloads/heatmap/rtl-sdr-misc/heatmap/heatmap.py --ytick 5m --palette charolastra "/home/anton/hf_noise/hf_monitoring_`date '+%d-%m-%Y'`.cvs" /var/www/hf_noise/images/hf_noise_`date '+%d-%m-%Y'`.png
 #echo "hf_noise_`date '+%d-%m-%Y'`.png"
 /home/anton/sh/create_thump_nail.sh
--- run_hf_noise_monitor_and_graph.sh
+++ run_hf_noise_monitor_and_graph.sh
@@ -1,4 +1,4 @@
 #Creating data for Image
-sudo /home/anton/Downloads/rtl_power/rtl-sdr/build/src/rtl_power -d4 -D2 -f1M:30M:10K -1 -g 50 >> "/home/anton/hf_noise/hf_monitoring_`date '+%d-%m-%Y'`.cvs"
+sudo /home/anton/Downloads/rtl_power/rtl-sdr/build/src/rtl_power -d2 -D2 -f1M:30M:10K -1 -g 50 >> "/home/anton/hf_noise/hf_monitoring_`date '+%d-%m-%Y'`.cvs"
 sleep 15
-sudo /home/anton/Downloads/rtl_power/rtl-sdr/build/src/rtl_power -d4 -D2 -f1M:30M:1M -1 -g 50 >> "/home/anton/hf_noise/hf_monitoring_graph_`date '+%d-%m-%Y'`.cvs"
+sudo /home/anton/Downloads/rtl_power/rtl-sdr/build/src/rtl_power -d2 -D2 -f1M:30M:1M -1 -g 50 >> "/home/anton/hf_noise/hf_monitoring_graph_`date '+%d-%m-%Y'`.cvs" 
