c9acf9ed3e561834dd3375ac3f6feaf015dc1b15
antonjan <anton.janovsky@gmail.com>
2017-11-01 21:52:39 +0000

    added images file

---
+++ graph/6Mhz_Power.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<?php include 'header.php';?>
+</head>
+<body>
+<div class="menu">
+<?php include 'menu.php';?>
+</div>
+<h2>ZR6AIC HF Noise Montoring</h2>
+<h2>7Mhz to 8Mhz 'Daily' Graph (5 Minute Average) 1Mhz bandwith</h2>
+<img src="1Mhz_Power_2_day_db.png">
+<p>
+<h2>1Mhs to 2Mhz `Weekly' Graph (30 Minute Average)</h2>
+<img src="1Mhz_Power_2_week_db.png">
+<p>
+<h2>1Mhs to 2Mhz `Monthly' Graph (2 Hour Average)</h2>
+<img src="1Mhz_Power_2_moths_db.png">
+<p>
+<h2>1Mhz to 2Mhz `Yearly' Graph (1 Day Average)</h2>
+<img src="1Mhz_Power_2_years_db.png">
+<p>
+<?php include 'footer.php';?>
+</body>
+</html>
