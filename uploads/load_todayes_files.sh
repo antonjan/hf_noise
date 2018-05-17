c578019f875b7a24680ffc9cde9a78c577d802bb
Anton <antonjan@users.noreply.github.com>
2018-05-17 09:16:21 +0000

    Fix path error

--- uploads/load_todayes_files.sh
+++ uploads/load_todayes_files.sh
@@ -1,3 +1,3 @@
 #hf_monitoring_10-05-2018.cvs
 #php ./compres_csv_file.php ./uploads/hf_monitoring_graph_21-01-2018_test.csv
-php ./compres_csv_file.php "../hf_monitoring_`date '+%d-%m-%Y'`.cvs"
+php ./compres_csv_file_arg.php "../hf_monitoring_graph_`date '+%d-%m-%Y'`.cvs"
