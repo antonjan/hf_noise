615384e3dc225ab03b06264e5f0374c9dacbdbb6
Anton <antonjan@users.noreply.github.com>
2018-08-12 23:31:55 +0200

    added master server url

--- images/index.php
+++ images/index.php
@@ -5,6 +5,7 @@ echo "<html><body>";
 echo "<h1>This is the automated HF Noise monitoring system operated by Anton Janovsky ZR6AIC.</h1>";
 echo "This system is using a RTL HF dongle available from <a href =\"http://www.giga.co.za/ocart/index.php?route=product/product&path=59_63&product_id=357\">Giga Technology</a><br>
 <a href =\"http://www.giga.co.za/ocart/index.php?route=product/product&path=59_63&product_id=357\"><img src=\"../graph/hf_sdr_1.jpg\" alt=\"100Khz to 1.7Ghz SDR\" style=\"width:304px;height:228px;\"></a><br>";
+echo "<h2>Master <a href ="http://rfnoise.amsatsa.org.za">RF Noise server</a></h2>";
 echo "<b>Click on Image below to enlarge the image and then again to zoom to maximum size, The scrowl baars can then be used to move the image</b><br>";
 echo "This system is taking a 10s snapshot at a 10Khz bandwith every 2 Minutes (10Khz per X and 2 minute per Y pixel)<br>";
 echo '0 - 30Mhz Kiwi HF sdr recever is <a href="http://zr6aic.giga.co.za:8073">avalible here</a><br>';
