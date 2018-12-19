5b2cfc25bb5ef4bfd3de84d8e25f03db7a877718
Anton <antonjan@users.noreply.github.com>
2018-12-19 12:29:49 +0200

    Adding Remote stastion Admin pages

---
+++ add_reg_station.php
@@ -0,0 +1,31 @@
+<?php
+echo "<html>";
+echo "<body>";
+include "menu.php";
+$Station_Call_sign  = $_POST['Station_Call_sign'];
+$Remote_station_coordinates_lat  = $_POST['Remote_station_coordinates_lat'];
+$Remote_station_coordinates_long  = $_POST['Remote_station_coordinates_long'];
+$Remote_station_login_Key = $_POST['Remote_Station_Key']; 
+$Remote_Filter_Enable = $_POST['Remote_Filter_Enable'];
+//************************************************ code *************************************************
+//
+$file = fopen("station_config.ini","w");
+//; This is a the Remote Station configuration file
+
+echo fwrite($file,"[station_details]\n");
+echo fwrite($file,"Call_sign = ".$Station_Call_sign."\n");
+echo fwrite($file,"Pass_Key = ".$Remote_station_login_Key."\n");
+echo fwrite($file,"GPS_Lat = ".$Remote_station_coordinates_lat."\n");
+echo fwrite($file,"GPS_Long = ".$Remote_station_coordinates_long."\n");
+echo fwrite($file,"Minute = 27\n");
+echo fwrite($file,"Hour = 23 ;Houre for upload\n");
+echo fwrite($file,"Bandwith = 1000 ;bandwith in Khz\n");
+echo fwrite($file,"Antenna_Switch = ".$Remote_Filter_Enable."\n"); //on ;Antenna Switch\n");
+
+fclose($file);
+
+echo "Thank You For Registering your Station<br>";
+echo "You station should now submit your daily data to the master server at about midnight 0:0 central africa time (GMT +2)";
+echo "</body>";
+echo "</html>";
+?>
--- menu.php
+++ menu.php
@@ -1,37 +1,15 @@
-Move the mouse over the button to open the Frequency selection menu or <a href="/hf_noise/images/">click here for Heatmap.</a><br>
-<div class="dropdown">
-  <button class="dropbtn">Frequency Selection</button>
-  <div class="dropdown-content">
- <a href="all_freq_2_Day.php">All frequency 2 Day graph</a>
- <a href="1Mhz_Power.php">1 to 2 Mhz graph</a>
- <a href="2Mhz_Power.php">2 to 3 Mhz graph</a>
- <a href="3Mhz_Power.php">3 to 4 Mhz graph</a>
- <a href="4Mhz_Power.php">4 to 5 Mhz graph</a>
- <a href="5Mhz_Power.php">5 to 6 Mhz graph</a>
- <a href="6Mhz_Power.php">6 to 7 Mhz graph</a>
- <a href="7Mhz_Power.php">7 to 8 Mhz graph</a>
- <a href="8Mhz_Power.php">8 to 9 Mhz graph</a>
- <a href="9Mhz_Power.php">9 to 10 Mhz graph</a>
- <a href="10Mhz_Power.php">10 to 11 Mhz graph</a>
- <a href="11Mhz_Power.php">11 to 12 Mhz graph</a>
- <a href="12Mhz_Power.php">12 to 13 Mhz graph</a>
- <a href="13Mhz_Power.php">13 to 14 Mhz graph</a>
- <a href="14Mhz_Power.php">14 to 15 Mhz graph</a>
- <a href="15Mhz_Power.php">15 to 16 Mhz graph</a>
- <a href="16Mhz_Power.php">16 to 17 Mhz graph</a>
- <a href="17Mhz_Power.php">17 to 18 Mhz graph</a>
- <a href="18Mhz_Power.php">18 to 19 Mhz graph</a>
- <a href="19Mhz_Power.php">19 to 20 Mhz graph</a>
- <a href="20Mhz_Power.php">20 to 21 Mhz graph</a>
- <a href="21Mhz_Power.php">21 to 22 Mhz graph</a>
- <a href="22Mhz_Power.php">22 to 23 Mhz graph</a>
- <a href="23Mhz_Power.php">23 to 24 Mhz graph</a>
- <a href="24Mhz_Power.php">24 to 25 Mhz graph</a>
- <a href="25Mhz_Power.php">25 to 26 Mhz graph</a>
- <a href="26Mhz_Power.php">26 to 27 Mhz graph</a>
- <a href="27Mhz_Power.php">27 to 28 Mhz graph</a>
- <a href="28Mhz_Power.php">28 to 29 Mhz graph</a>
- <a href="29Mhz_Power.php">29 to 30 Mhz graph</a>
-  </div>
-</div>
+<?php
+//echo "<html>";
+//echo "<head>";
+//echo "<title>Edit Remote Station</title>";
+//echo "</head>";
+//echo "<body>";
+echo "[<a href=\"index.php\">Main</a>][<a href=\"register_your_station.php\">Register your Station</a>][<a href=\"\hf_noise/dygraphs\">Graph Beata test</a>][<a href=\"\hf_noise/date_range_picker/index.php\">List Export</a>][<a href=\"\about_us.html\">About US</a>][<a href=\"https://docs.google.com/forms/d/e/1FAIpQLSfgOJTti_9YisJ3fNSWnff091n61o2643SRkxXJTh_YeODeXA/viewform?formkey=dEZUN2tTUTU1NUdweUxldkcwNmpXNlE6MQ&fromEmail=true\">Contact US</a>]";
 
+//echo "[<a href=\"\\index.php\">Main</a>][<a href=\"\\register_station.php\">Register your Station</a>][<a href=\"\\hf_noise\index.php\">Graph</a>][<a href=\"\\hf_noise\\date_range_picker\\index.php\">List</a>][<a href=\"\\hf_noise\\date_range_picker\\export.php\">Export</a>][<a href=\"\\about_us.html\">About US</a>][Contact US]";
+
+
+echo "<hr>";
+//echo "</body>";
+//echo "</html>";
+?>
