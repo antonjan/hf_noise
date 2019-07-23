4caa0e66c02f094866eb1bdfc8593aaf7bb17029
Anton <antonjan@users.noreply.github.com>
2019-07-23 17:54:33 +0200

    adding USB port reseting

---
+++ check_if_file_was_updated.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+NOW=$(date +"%m-%d-%Y:%H:%M")
+echo "now " $NOW
+NOW2=$(date -r "/home/hfnoise/hf_noise/hf_monitoring_graph_`date '+%d-%m-%Y'`.cvs" +"%m-%d-%Y:%H:%M")
+echo "now2" $NOW2
+#todate=$(date -d 2013-07-18 +"%Y%m%d")  # = 20130718
+#cond=$(date -d 2013-07-15 +"%Y%m%d")    # = 20130715
+if [ "$NOW" == "$NOW2" ] ; then
+echo 'is the same'
+else
+echo 'Is not the same'
+usb_file=$(sudo lsof -w |grep rtl_power |grep /dev/bus |  awk -F" " '{print $9}')
+echo $usb_file
+sudo /home/hfnoise/Downloads/usbreset/usbreset $usb_file
+lsusb
+#sudo lsof | grep rtl_power | grep /dev/bus
+#/dev/bus/usb/002/010``
+#sudo killall rtl_power
+fi
