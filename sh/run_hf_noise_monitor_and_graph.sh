84a9fcc5215b87e739d2fdfcedb8cfa62b9a70df
Anton <antonjan@users.noreply.github.com>
2019-07-25 20:47:11 +0200

    fixing path

--- sh/run_hf_noise_monitor_and_graph.sh
+++ sh/run_hf_noise_monitor_and_graph.sh
@@ -1,6 +1,6 @@
 #Creating data for Image
-sudo /media/ubuntu/casper-rw/upper/home/hfnoise/rtl-sdr/build/src/rtl_power -d0 -D2 -f1M:30M:10K -1 -g 50 >> "/home/hfnoise/hf_noise/hf_monitoring_`date '+%d-%m-%Y'`.cvs"
+sudo /home/hfnoise/rtl-sdr/build/src/rtl_power -d0 -D2 -f1M:30M:10K -1 -g 50 >> "/home/hfnoise/hf_noise/hf_monitoring_`date '+%d-%m-%Y'`.cvs"
 sleep 15
-sudo /media/ubuntu/casper-rw/upper/home/hfnoise/rtl-sdr/build/src/rtl_power -d0 -D2 -f1M:30M:1M -1 -g 50 >> "/home/hfnoise/hf_noise/hf_monitoring_graph_`date '+%d-%m-%Y'`.cvs" 
+sudo /home/hfnoise/rtl-sdr/build/src/rtl_power -d0 -D2 -f1M:30M:1M -1 -g 50 >> "/home/hfnoise/hf_noise/hf_monitoring_graph_`date '+%d-%m-%Y'`.cvs" 
 #Check if file updated if not reset usb port
 sudo /home/hfnoise/hf_noise/sh/check_if_file_was_updated.sh
